c10be24b21f48f244ad3831bcc48bc48e2c53adf
Joshua Albert <albert@strw.leidenuniv.nl>
2019-07-23 22:03:43 +0300

    Bug (typo was ** instead of *)

--- rayintegral_kernels/tests/test_kernels.py
+++ rayintegral_kernels/tests/test_kernels.py
@@ -1,3 +1,8 @@
+import numpy as np
+import tensorflow as tf
+
+from rayintegral_kernels import float_type
+from rayintegral_kernels.kernel import RandomKernel, M52, TrapezoidKernel
 from .common_setup import *
 
 from ..kernel import RandomKernel, TrapezoidKernel, RBF, RQ
@@ -33,3 +38,75 @@ def test_RQ(tf_session):
 
         plt.legend()
         plt.show()
+
+
+def test_rbf_taylor():
+    with tf.Session(graph=tf.Graph()) as sess:
+        N = 2
+        x = tf.constant([[0., 0., t] for t in np.linspace(0., 60., N)], float_type)
+        k = tf.constant([[0., np.sin(theta),np.cos(theta)] for theta in np.linspace(-4.*np.pi/180., 4.*np.pi/180., N)], float_type)
+        k /= tf.linalg.norm(k, axis=1, keepdims=True)
+        #
+        X = tf.concat([k,x],axis=1)
+        theta = tf.constant([1., 10.],float_type)
+        a = tf.constant(200., float_type)
+        b = tf.constant(100., float_type)
+        mu = None
+        ref_location = X[0,3:6]
+        ref_direction = X[0,0:3]
+
+        ref_kern = RandomKernel(M52(theta), 2000, a, b, mu=mu, ref_location=ref_location, ref_direction=ref_direction,
+                                obs_type='DTEC', ionosphere_type='flat')
+        ref_K = ref_kern.K(X, X)
+
+        ref_g = tf.gradients(ref_K,[theta])[0]
+        ref_K = sess.run(ref_K)
+        ref_g = sess.run(ref_g)
+        F = []
+        R = [4,5,6,7,8,9]
+        import pylab as plt
+        plt.imshow(ref_K)
+        plt.colorbar()
+        plt.show()
+        for res in R:
+            test_kern = TrapezoidKernel(M52(theta), res, a, b, mu=mu, ref_location=ref_location,
+                                     ref_direction=ref_direction,
+                                     obs_type='DTEC', ionosphere_type='flat')
+            from timeit import default_timer
+            K = test_kern.K(X, X)
+            g = tf.gradients(K,[theta])[0]
+            t0 = default_timer()
+            K = sess.run(K)
+            print(K)
+            print((default_timer()-t0))
+            t0 = default_timer()
+            g = sess.run(g)
+            print((default_timer() - t0))
+            plt.imshow(K)
+            plt.colorbar()
+            plt.show()
+            print(ref_g,g, ref_g-g)
+
+            f = np.mean(np.abs(ref_K - K))
+            F.append(f)
+
+        plt.plot(R, F)
+
+        plt.show()
+
+        # ref_kern = RandomKernel(RBF(theta), 2000, a, b, mu=mu, ref_location = ref_location, ref_direction = ref_direction, obs_type='DTEC', ionosphere_type='flat')
+        # g = tf.gradients(tf.reduce_sum(ref_kern.K(X,X)), [theta])[0]
+        # ref_g = sess.run(g)
+        # F = []
+        # R = [2,3,4,5]
+        # for res in R:
+        #     test_kern = TaylorKernel(RBF(theta), res, a, b, mu=mu, ref_location=ref_location, ref_direction=ref_direction,
+        #                  obs_type='DTEC', ionosphere_type='flat')
+        #     g = tf.gradients(tf.reduce_sum(test_kern.K(X, X)), [theta])[0]
+        #
+        #     f = np.mean(np.abs(ref_g - sess.run(g)))
+        #     F.append(f)
+        # import pylab as plt
+        # plt.plot(R,F)
+        #
+        # plt.show()
